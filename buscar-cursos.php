ecf9ba0ef2cf7dc5104fe16faeffe91ed9ee944b
Lucian <lucianjosegoncales@gmail.com>
2025-10-29 13:16:56 -0300

    primeira versão do webcrawler.

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -8,17 +8,20 @@ use Symfony\Component\DomCrawler\Crawler;
 $client = new Client();
 $res = $client->request('GET', 'https://www.alura.com.br/carreiras/desenvolvimento-backend-php/');
 
+
 $html = $res->getBody();
+//var_dump($html);
+
 //echo $html;
 
-$crawler = new Crawler();
+$crawler = new Crawler($html);
 
-$crawler->addHtmlContent($html);
-
-$cursos = $crawler->filter('span.course-card__title');
-
+$cursos = $crawler->filter('h5.course-card__title');
 //var_dump($cursos);
 
+//$cursos = $crawler->filter('span.course-card__title');
+
+
 foreach ($cursos as $curso) {
-    echo $curso->textContent . PHP_EOL;
+    echo $curso->nodeValue . PHP_EOL;
 }
